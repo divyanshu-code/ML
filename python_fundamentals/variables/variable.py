5a0dae0efa4ad0fa7777b4fda9a8d3e8341ec946
divyanshu <divyanshubisht5734@gmail.com>
2026-06-07 10:28:53 +0530

    Add variable manipulation and logic demonstration in variable.py

---
+++ python_fundamentals/variables/variable.py
@@ -0,0 +1,25 @@
+print("hello divyanshu")
+
+age = None
+
+'''print(age + 10)'''
+
+# print(type(age))
+
+age = 3 
+Age = 3.5
+AGE=  7 
+
+# print( not 3.5 > 3)
+
+string = "divyanshu"
+
+def logic () :
+ for i in range(1 ,10 , 2):
+    print(i)
+
+logic()
+
+sum = lambda a , b , c  : a*b+c
+
+print("Sum is:", sum(2,3,4))
